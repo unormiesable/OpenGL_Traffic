40937ea737b80b20889920df1d22ba936cc093c2
sxvxn <unormiesable@gmail.com>
2025-05-02 07:17:38 +0700

    cleaning

--- scene.py
+++ scene.py
@@ -24,25 +24,9 @@ class Scene:
         app = self.app
         add = self.add_object
         
-        # TEST DENGAN PRIMITIVE OBJECT
-        
-        # TEXTURED
-        # add(Cube(app, pos=(0, 1, 0), scale=(1, 1, 1)))
-        # add(Plane(app, pos=(0, 0, 0), uni_scale= 5))
-        
-        # NO TEXTURED
+        # JALAN
         add(ColorPlane(app, pos=(0, 0, 0), color=(0.1, 0.1, 0.1), scale=(2, 1, 1), uni_scale= 10))
-        # add(ColorCube(app, pos=(0, 2, 0), color=(1.0, 0.0, 0.0), scale=(1, 2, 1)))
-        # add(ColorCylinder(app, pos=(0, 1, 0), color=(0.2, 0.2, 0.2), scale=(1, 1, 1), rot=(90, 0, 0)))
-        
-        # TEST DENGAN FILE OBJ (PROTOTYPING)
-        # add(Gate(app, pos=(0, 0, 0)))
-        # add(Yellow_Car(app, pos=(0, 0, 0), uni_scale= 0.8))
-        
-        # TESTING LOOP RANDOM COLOR
-        # for i in range(10):
-        #     add(ColorCube(app, pos=((i+1)-5, (i+1)/5, 0), color=(random.uniform(0.0, 1.0), random.uniform(0.0, 1.0), random.uniform(0.0, 1.0)), scale=(0.5, (i+1)/5, 1)))
-    
+
         # GARIS JALAN
         for i in range(8):
             add(ColorCube(self.app, pos=(-16 + (i * 5), 0, 0), scale=(1, 0.05, 0.05)))
@@ -51,18 +35,6 @@ class Scene:
         add(ColorCube(self.app, pos=(0, 0, 12), scale=(20, 0.1, 2), color=(0.3, 0.3, 0.3)))
         add(ColorCube(self.app, pos=(0, 0, -12), scale=(20, 0.1, 2), color=(0.3, 0.3, 0.3)))
     
-        # MOBIL MOBIL (TESTING CLASS MOBIL (MASIH PERLU DIPERBAIKI))
-        # for i in range(5):
-        #     car_position = (-16 + (i * 8) + random.uniform(0.0, 2.0), 0, random.randint(-7, 7))
-        #     car_color = (random.uniform(0.0, 1.0), random.uniform(0.0, 1.0), random.uniform(0.0, 1.0))
-        #     Car(self.app, add ,position=car_position, color=car_color)
-        
-        # for i in range(3):
-        #     add(Fixed_Car(app, pos=(-10 + (i * 10), 0, random.randint(-7, 7)),
-        #                   color=(random.uniform(0.0, 1.0), random.uniform(0.0, 1.0), random.uniform(0.0, 1.0)),
-        #                   sec_color=(random.uniform(0.0, 1.0), random.uniform(0.0, 1.0), random.uniform(0.0, 1.0))))
-        
-        # TAMBAHIN MOBIL RANDOM SEGALANYA
         for i in range(4):
             car = Fixed_Car(app, pos=(-5 + (i * 6), 0, random.uniform(-8.0, 8.0)),
                             color=(random.uniform(0.0, 0.8), random.uniform(0.0, 0.8), random.uniform(0.0, 0.8)),
@@ -84,16 +56,7 @@ class Scene:
             
         # ADD DASAR
         add(ColorPlane(app, pos=(0, -0.02, 0), uni_scale = 30, color=(0.39, 0.26, 0.13)))
-        
 
-        # SETUP SCENE
-        # add(ColorCube(app, color=(0.1, 0.1, 0.1), scale=(10, 0.05, 1)))
-        # add(ColorCube(app, color=(0.1, 0.1, 0.1), scale=(10, 0.05, 1), rot=(0, 90, 0)))
-        
-        # add(ColorCube(app, color=(0.3, 0.3, 0.3), scale=(1, 0.1/4, 1), pos=(5, 0, 5), uni_scale=4.5))
-        
-        # TEST TRAFFIC LIGHT
-        # add(Traffic_Light(app))
         
     # SISTEM ANIMASI (MASIH BETA)
     def update(self):
@@ -117,4 +80,4 @@ class Scene:
                 
                 car.update()
                 
-        animate_cars()
+        # animate_cars()
