b216e8d458192fc362392b3129f8a0e2c31537d7
sxvxn <unormiesable@gmail.com>
2025-05-26 21:46:03 +0700

    pybullet for physics

--- scene.py
+++ scene.py
@@ -24,18 +24,13 @@ class Scene:
         
         self.objs = []
         
-        self.main_obj = ColorCube(app, pos=(0, 2, 0), rot=(0, random.randint(0, 360), 0))
+        self.main_obj = ColorCube(app, pos=(0, 1, 0), rot=(0, random.randint(0, 360), 0))
         self.objs.append(self.main_obj)
         add(self.main_obj)
-        
-        add(ColorPlane(app, pos=(0, 0, 0), color=(0.3, 0.5, 0.3), uni_scale=10, specularity=0.1))
+    
             
     
     # SISTEM ANIMASI (MASIH BETA)
     def update(self):
-        for o in self.objs:
-            o.pos = glm.vec3(o.pos[0], 2 + math.sin( (self.app.time) * 2) * 0.8, o.pos[2])
-            o.rot.y += 0.01
-            o.update()
-
+        pass
                 
