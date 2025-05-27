5e79a50388b80417ef54105506ae27314908452f
sxvxn <unormiesable@gmail.com>
2025-05-28 05:35:39 +0700

    cleaning

--- scene.py
+++ scene.py
@@ -23,25 +23,19 @@ class Scene:
         app = self.app
         add = self.add_object
         
-        self.objs = []
+        Plane = ColorPlane(app, uni_scale=5)
+        Plane.anim = Animation(Plane, app)
         
-        self.Cube_001 = Fixed_Car(app, pos=(0, 1, 0), rot=(0, 0, 0), color=(0.2, 0.8, 0.2))
-        self.Cube_001.anim = Animation(self.Cube_001, app)
-        self.objs.append(self.Cube_001)
+        Plane.anim.add_keyframe(rot=(0, 0, 0), time=0)
+        Plane.anim.add_keyframe(rot=(0, 180, 0), time=2)
         
-        a = self.Cube_001.anim
-        a.add_keyframe(pos=(0, 1, 0), rot=(0, 0, 0), scale=(1, 1, 1), time=3)
-        a.add_keyframe(pos=(3, 1, 0), rot=(0, 180, 0), scale=(1, 1, 1), time=6)
-        a.add_keyframe(pos=(-3, 1, 0), rot=(0, 360, 0), scale=(1, 1, 1), time=9)
+        add(Plane)
         
-        add(self.Cube_001)
-        
-        self.Plane_001 = ColorPlane(app, pos=(0, 0, 0), rot=(0, 0, 0), color=(0.3, 0.3, 0.5), uni_scale=5)
-        self.objs.append(self.Plane_001)
-        add(self.Plane_001)
                 
     # SISTEM ANIMASI (MASIH BETA)
     def update(self):
-        self.Cube_001.anim.animate()
+        for o in self.objects:
+            if hasattr(o, 'anim'):
+                o.anim.animate()
         
                 
