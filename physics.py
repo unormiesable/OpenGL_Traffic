82b2f5b3222c1567ccd8870e6a949d51c6813c98
sxvxn <unormiesable@gmail.com>
2025-06-01 05:02:27 +0700

    better physics (non uniform scaling)

--- physics.py
+++ physics.py
@@ -9,10 +9,24 @@ class Physics:
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0, 0, -9.81 * 5)
         
-    def add_physics(self, urdf, basepos=(0, 0, 0), baserot=(0, 0, 0), scale=1):
+    def add_physics_urdf(self, urdf, basepos=(0, 0, 0), baserot=(0, 0, 0), scale=1):
         return p.loadURDF(urdf, basePosition=(basepos[0], basepos[2], basepos[1]),
                           baseOrientation=p.getQuaternionFromEuler((baserot[0], baserot[2], baserot[1])),
                           globalScaling=scale * 2)
+    
+    def add_physics_geometry(self, geometry=p.GEOM_BOX ,half_extents=(0.5, 0.5, 0.5), pos=(0, 0, 0), rot=(0, 0, 0), mass=1, scale=(1, 1, 1)):
+        collision_shape = p.createCollisionShape(geometry, halfExtents=half_extents, meshScale=(scale[0], scale[2], scale[1]))
+        visual_shape = p.createVisualShape(geometry, halfExtents=half_extents, rgbaColor=[1, 0, 0, 1], meshScale=(scale[0], scale[2], scale[1]))
+        
+        orientation = p.getQuaternionFromEuler((rot[0], rot[2], rot[1]))
+        body = p.createMultiBody(baseMass=mass,
+                                baseCollisionShapeIndex=collision_shape,
+                                baseVisualShapeIndex=visual_shape,
+                                basePosition=(pos[0], pos[2], pos[1]),
+                                baseOrientation=orientation)
+        return body
+
+    
     
     def get_pos(self, object):
         res = p.getBasePositionAndOrientation(object)
--- scene.py
+++ scene.py
@@ -5,6 +5,8 @@ from pyglm import glm
 import random
 import math
 
+import pybullet as p
+
 # SCENE (PENATAAN OBJEK OBJEK PADA SCENE)
 class Scene:
     def __init__(self, app):
@@ -24,45 +26,45 @@ class Scene:
         add = self.add_object
         
         Plane = ColorPlane(app, uni_scale=15, specularity=1, pos=(0, 0, 0), color=(0.2, 0.2, 0.2))
-        self.app.physics.add_physics('plane.urdf', Plane.pos, (0, 0, 0))
+        self.app.physics.add_physics_urdf('plane.urdf', Plane.pos, (0, 0, 0))
         add(Plane)
         
 
-        # self.Cube_001 = ColorCube(app, pos=(0, 3, 0), rot=(0, 0, 0), color=(0.8, 0.3, 0.3), uni_scale=0.5,
-        #                           specularity=1.0, metalness=0)
-        # self.Cube_001_phy = self.app.physics.add_physics('cube.urdf', self.Cube_001.pos, self.Cube_001.rot, scale=0.5)
-        # self.Cube_001.physics = self.Cube_001_phy
-        # add(self.Cube_001)
-        
-        self.Cube_001 = ColorCube(app, pos=(0.5, 5, 0), rot=(0, 35, 20), color=(0.8, 0.3, 0.3), uni_scale=0.5,
+        self.Cube_001 = ColorCube(app, pos=(0, 3, 0), rot=(90, -15, -35), color=(0.8, 0.3, 0.3), uni_scale=0.5, scale=(1, 2, 1),
                                   specularity=1.0, metalness=0)
-        self.Cube_001_phy = self.app.physics.add_physics('cube.urdf', self.Cube_001.pos, self.Cube_001.rot, scale=0.5)
-        self.Cube_001.physics = self.Cube_001_phy
         add(self.Cube_001)
         
-        self.Cube_002 = ColorCube(app, pos=(0, 4, 0), rot=(0, 0, 0), uni_scale=0.5, color=(0.2, 0.4, 0.5),
+        self.Cube_001_phy = self.app.physics.add_physics_geometry(geometry=p.GEOM_BOX,
+                                                    half_extents=(self.Cube_001.scale[0], self.Cube_001.scale[2], self.Cube_001.scale[1]),
+                                                    pos=self.Cube_001.pos,
+                                                    rot=self.Cube_001.rot,
+                                                    mass=1)
+        self.Cube_001.physics = self.Cube_001_phy
+        
+        
+        self.Cube_002 = ColorCube(app, pos=(0, 5, 0), rot=(45, 20, 0), color=(0.3, 0.8, 0.3), uni_scale=0.5, scale=(1, 2, 1),
                                   specularity=1.0, metalness=0)
-        self.Cube_002_phy = self.app.physics.add_physics('cube.urdf', self.Cube_002.pos, self.Cube_002.rot, scale=0.5)
-        self.Cube_002.physics = self.Cube_002_phy
         add(self.Cube_002)
         
-        self.Cube_003 = ColorCube(app, pos=(-0.5, 7, 0.2), rot=(30, 25, 22), uni_scale=0.5, color=(0.4, 0.4, 0.2),
-                                  specularity=1.0, metalness=0)
-        self.Cube_003_phy = self.app.physics.add_physics('cube.urdf', self.Cube_003.pos, self.Cube_003.rot, scale=0.5)
-        self.Cube_003.physics = self.Cube_003_phy
-        add(self.Cube_003)
+        self.Cube_002_phy = self.app.physics.add_physics_geometry(geometry=p.GEOM_BOX,
+                                                    half_extents=(self.Cube_002.scale[0], self.Cube_002.scale[2], self.Cube_002.scale[1]),
+                                                    pos=self.Cube_002.pos,
+                                                    rot=self.Cube_002.rot,
+                                                    mass=1)
+        self.Cube_002.physics = self.Cube_002_phy
         
-        self.Sphere_001 = ColorSphere(app, pos=(-0.5, 7, 0.2), rot=(0, 0, 0), uni_scale=0.5, color=(0.5, 0.2, 0.4),
+        
+        self.Sphere_001 = ColorSphere(app, pos=(0.5, 4, 0), rot=(0, 0, 0), color=(0.3, 0.8, 0.3), uni_scale=0.5, scale=(1, 1, 1),
                                   specularity=1.0, metalness=0)
-        self.Sphere_001_phy = self.app.physics.add_physics('sphere2.urdf', self.Sphere_001.pos, self.Sphere_001.rot, scale=0.5)
-        self.Sphere_001.physics = self.Sphere_001_phy
         add(self.Sphere_001)
         
-        self.Sphere_002 = ColorSphere(app, pos=(0, 1.2, 0), rot=(0, 0, 0), uni_scale=0.5, color=(0.3, 0.3, 0.1),
-                                  specularity=1.0, metalness=0)
-        self.Sphere_002_phy = self.app.physics.add_physics('sphere2.urdf', self.Sphere_002.pos, self.Sphere_002.rot, scale=0.5)
-        self.Sphere_002.physics = self.Sphere_002_phy
-        add(self.Sphere_002)
+        self.Sphere_001_phy = self.app.physics.add_physics_geometry(geometry=p.GEOM_SPHERE,
+                                                    scale=self.Sphere_001.scale,
+                                                    pos=self.Sphere_001.pos,
+                                                    rot=self.Sphere_001.rot,
+                                                    mass=1)
+        self.Sphere_001.physics = self.Sphere_001_phy
+
 
         # add(ColorCube(app, pos=(0, 0, 0), color=(0, 0, 1), uni_scale=0.1, specularity=1.0, metalness=0))
         
