86d5b90fe66f740462dfc5dbdb5828d4c9c79089
sxvxn <unormiesable@gmail.com>
2025-05-31 07:05:59 +0700

    physics still funky

--- physics.py
+++ physics.py
@@ -7,27 +7,28 @@ class Physics:
     def __init__(self):
         self.client = p.connect(p.DIRECT)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
-        p.setGravity(0, 0, -9.81)
+        p.setGravity(0, 0, -9.81 * 10)
         
-    def add_physics(self, urdf, basepos, baserot, scale=2):
+    def add_physics(self, urdf, basepos=(0, 0, 0), baserot=(0, 0, 0), scale=1):
         return p.loadURDF(urdf, basePosition=(basepos[0], basepos[2], basepos[1]),
-                          baseOrientation=p.getQuaternionFromEuler(baserot),
-                          globalScaling=scale)
+                          baseOrientation=p.getQuaternionFromEuler((baserot[0], baserot[2], baserot[1])),
+                          globalScaling=scale * 2)
     
     def get_pos(self, object):
         res = p.getBasePositionAndOrientation(object)
         pos = glm.vec3(res[0][0], res[0][2], res[0][1])
         return pos
     
-    def get_rot(self, object):
-        res = p.getBasePositionAndOrientation(object)
-        rot = glm.vec3(res[1][0], res[1][2], res[1][1])
-        return rot
+    def get_rot(self, physics_object):
+        res = p.getBasePositionAndOrientation(physics_object)
+        rot = p.getEulerFromQuaternion(res[1])
+        fixed_rot = glm.vec3(-rot[0], -rot[2], -rot[1])
+        
+        return fixed_rot
     
     def get_status(self, object):
-        res = p.getBasePositionAndOrientation(object)
-        pos = glm.vec3(res[0][0], res[0][2], res[0][1])
-        rot = glm.vec3(res[1][0], res[1][2], res[1][1])
+        rot = self.get_rot(object)
+        pos = self.get_pos(object)
         return pos, rot
     
     def stepSimulation(self):
--- scene.py
+++ scene.py
@@ -23,22 +23,35 @@ class Scene:
         app = self.app
         add = self.add_object
         
-        Plane = ColorPlane(app, uni_scale=5, specularity=0, pos=(0, 0, 0))
+        Plane = ColorPlane(app, uni_scale=10, specularity=0, pos=(0, 0, 0))
         self.app.physics.add_physics('plane.urdf', Plane.pos, (0, 0, 0))
         add(Plane)
         
-        self.Cube_001 = ColorCube(app, pos=(0, 5, 0), specularity=1.0, metalness=0)
-        self.Cube_001_phy = self.app.physics.add_physics('cube.urdf', self.Cube_001.pos, (0, 0, 0))
+        self.Cube_001 = ColorCube(app, pos=(0.5, 5, 0), rot=(0, 0, 0), color=(0.8, 0.3, 0.3), uni_scale=0.5,
+                                  specularity=1.0, metalness=0)
+        self.Cube_001_phy = self.app.physics.add_physics('cube.urdf', self.Cube_001.pos, self.Cube_001.rot, scale=0.5)
+        self.Cube_001.physics = self.Cube_001_phy
         add(self.Cube_001)
+        
+        
+        self.Cube_002 = ColorCube(app, pos=(0, 3, 0), rot=(0, 0, 0), uni_scale=0.5,
+                                  specularity=1.0, metalness=0)
+        self.Cube_002_phy = self.app.physics.add_physics('cube.urdf', self.Cube_002.pos, self.Cube_002.rot, scale=0.5)
+        self.Cube_002.physics = self.Cube_002_phy
+        add(self.Cube_002)
+
+        add(ColorCube(app, pos=(5, 0, 0), color=(0, 0, 1)))
         
                 
     # SISTEM ANIMASI (MASIH BETA)
     def update(self):
         for o in self.objects:
-            if hasattr(o, 'anim') or hasattr(o, 'physics'):
+            if hasattr(o, 'anim'):
                 o.anim.animate()
-    
-        print(self.app.physics.get_pos(self.Cube_001_phy))
-        self.Cube_001.pos = (self.app.physics.get_pos(self.Cube_001_phy))
+            
+            if hasattr(o, 'physics'):
+                o.pos = (self.app.physics.get_pos(o.physics))
+                o.rot = (self.app.physics.get_rot(o.physics))
+                o.animate()
         
-        self.Cube_001.animate()
+        print(self.Cube_001.pos, self.Cube_001.rot)
